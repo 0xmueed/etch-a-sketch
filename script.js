7ba196052e5ad6eb89589e17e0edd139db125a87
Abdul Mueed Malik <abdulmueedmalik@disroot.org>
2026-02-22 20:07:01 +0500

    Add container div and createGrid() function

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+function createGrid(m, n, parent)
+{
+    for (let i = 0; i < m; i++) {
+	let columnDiv = document.createElement("div");
+	columnDiv.className = "col";
+	columnDiv.style.flex = "1";
+	for (let i = 0; i < n; i++) {
+	    let rowDiv = document.createElement("div");
+	    rowDiv.className = "row";
+	    rowDiv.style.flex = "1";
+	    rowDiv.style.height = "30px"
+	    rowDiv.style.backgroundColor = "white";
+	    rowDiv.style.textAlign = "center";
+	    rowDiv.style.border = "1px black solid";
+	    columnDiv.appendChild(rowDiv);
+	}
+	parent.appendChild(columnDiv);
+    }
+}
+
 let container = document.getElementById('container');
+createGrid(16, 16, container);
