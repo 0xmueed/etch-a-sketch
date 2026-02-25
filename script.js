8fb2618755172af90ec40229444313bc0d454799
Abdul Mueed Malik <abdulmueedmalik@disroot.org>
2026-02-25 22:23:11 +0500

    Add functionality to set custom row/column for the grid

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-function createGrid(m, n, parent)
+function createGrid(rows, columns, parent)
 {
-    for (let i = 0; i < m; i++) {
+    parent.replaceChildren();
+    for (let i = 0; i < columns; i++) {
 	let columnDiv = document.createElement("div");
 	columnDiv.className = "col";
 	columnDiv.style.flex = "1";
-	for (let i = 0; i < n; i++) {
+	for (let i = 0; i < rows; i++) {
 	    let rowDiv = document.createElement("div");
 	    rowDiv.className = "row";
 	    rowDiv.style.flex = "1";
 	    rowDiv.style.height = "30px"
 	    rowDiv.style.backgroundColor = "white";
 	    rowDiv.style.textAlign = "center";
-	    rowDiv.style.border = "1px black solid";
+	    rowDiv.style.border = "1px lightgray dotted";
 	    columnDiv.appendChild(rowDiv);
 	}
 	parent.appendChild(columnDiv);
@@ -19,4 +20,25 @@ function createGrid(m, n, parent)
 }
 
 let container = document.getElementById('container');
-createGrid(16, 16, container);
+let rowInput = document.getElementById('row-input');
+let columnInput = document.getElementById('column-input');
+let goButton = document.getElementById('go-button');
+
+// Change background color of target element when mouse hovers over container:
+container.addEventListener('mouseover', (e) => {
+    let target = e.target;
+    if (target.className == "row")
+	target.style.backgroundColor = "lightyellow";
+});
+
+
+// Default grid (when page refreshes):
+createGrid(10, 50, container);
+
+// Custom grid (set by user):
+let rowNum, columnNum;
+goButton.addEventListener('click', () => {
+    rowNum = Math.abs(rowInput.value);
+    columnNum = Math.abs(columnInput.value);
+    createGrid(rowNum, columnNum, container);
+});
